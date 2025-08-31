593ce2cb90e816989c02231a37ff69f274bccff7
fernadev14 <fernandoperez.99a@gmail.com>
2025-08-31 14:53:53 -0500

    update footer

--- public/js/components/footer.js
+++ public/js/components/footer.js
@@ -11,7 +11,7 @@ const footerTemplate = `
       </ul>
     </div>
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#" class="hover:underline">FernaShop</a>. Casi todos los derechos reservados.</span>
+    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#" class="hover:underline">FernaShop</a>. Proyecto con JavaScript puro.</span>
   </div>
 </footer>
 `;
